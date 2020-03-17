1fe9d94143a295f010d857876100edc768d5d513
Chris <ChrisNosowsky@gmail.com>
2020-03-17 18:37:16 -0400

    Prettier added to code

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,21 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-
-const address = process.argv[2]
+const address = process.argv[2];
 
 if (!address) {
-    console.log('Please provide a location')
-}
-else {
-    geocode(address, (error, {latitude, longitude, location}) => {
-        if (error) {
-            return console.log(error)
-        }
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return console.log(error)
-            }
-            console.log(location)
-            console.log(forecastData)
-        })
-    })
-
+  console.log('Please provide a location');
+} else {
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
 }
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,22 +1,29 @@
-const request = require('request')
-
-
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/2aa76798df1afb5334c9e9bbf8a62416/' + latitude + ',' + longitude
+  const url =
+    'https://api.darksky.net/forecast/2aa76798df1afb5334c9e9bbf8a62416/' +
+    latitude +
+    ',' +
+    longitude;
 
-    request({url, json:true}, (error, {body}) => {
-        if(error) {
-            callback('Unable to connect to weather service!', undefined)
-        }
-        else if (body.error) {
-            callback('Unable to find location. Try another search.', undefined)
-        }
-        else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " .There is a " + body.currently.precipProbability * 100 + "% chance of rain.")
-        }
-    })
-}
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location. Try another search.', undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' It is currently ' +
+          body.currently.temperature +
+          ' .There is a ' +
+          body.currently.precipProbability * 100 +
+          '% chance of rain.'
+      );
+    }
+  });
+};
 
-
-module.exports = forecast
+module.exports = forecast;
