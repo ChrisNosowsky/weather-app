cc6e7bc701240d3302531f0ccfb98bc42f677b7e
Chris <ChrisNosowsky@gmail.com>
2020-03-17 19:51:55 -0400

    Wiring up geocode and forecast api calls to expressJS /weather endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast')
 const app = express();
 
 // Define paths for Express config
@@ -39,11 +40,45 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'It is clear',
-    location: 'Walled Lake, MI',
-  });
+  if(!req.query.address) {
+    return res.send({
+      error: "Address must be provided!"
+    })
+  }
+
+
+  geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    if(error) {
+      return res.send({ error }) // short hand
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      })
+    })
+  })
 });
+
+
+app.get('/products', (req, res) => {
+  if(!req.query.search) {
+    return res.send({
+      error: "you must provide a search term"
+    })
+  }
+
+  console.log(req.query.search)
+  res.send({
+    products: []
+  })
+})
 
 app.get('/help/*', (req, res) => {
   res.render('404page', {
