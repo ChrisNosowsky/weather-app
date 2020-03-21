4975fd465391da2676bac99df3145fe611c7c1e2
Chris <ChrisNosowsky@gmail.com>
2020-03-20 23:32:18 -0400

    Client side javascript

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
-console.log("LOADING");
+console.log("Client side javascript file is loaded!");
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error)
+        }
+        else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
