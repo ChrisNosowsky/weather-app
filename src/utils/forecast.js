e1c61d8193e720b7e4c5fcdccd2d7caabfd3d2dc
Chris <ChrisNosowsky@gmail.com>
2020-04-01 01:19:19 -0400

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,8 +17,12 @@ const forecast = (latitude, longitude, callback) => {
         undefined,
         body.daily.data[0].summary +
           ' It is currently ' +
-          body.currently.temperature +
-          ' .There is a ' +
+          body.currently.temperature + 
+          ' degrees out. The high today is ' + 
+          body.daily.data[0].temperatureHigh + 
+          ' with a low of ' + 
+          body.daily.data[0].temperatureLow +
+          '. There is a ' +
           body.currently.precipProbability * 100 +
           '% chance of rain.'
       );
